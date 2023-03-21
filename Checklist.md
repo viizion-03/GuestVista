c8cb91bd07b557c24d45cdc123a1ea2b220def8e
Kevin <125473461+viizion-03@users.noreply.github.com>
2023-03-21 06:35:51 +0200

    Creating firebase.js using .env

---
+++ Checklist.md
@@ -0,0 +1,7 @@
+fix styling of login
+make an auth context with all the goodies
+clean up login form
+
+Context:
+-login/register/signout fn
+-
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,24 +1,15 @@
-
-import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { initializeApp } from "firebase/app";
 
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.envFIREBASE_APP_ID
+};
 
-const firebaseConfig = {//firebase config object
-    apiKey: "AIzaSyAM6XelTYtg4EaKvYM4L6k96FMI3IDSftw",
-    authDomain: "guestvista-4308f.firebaseapp.com",
-    projectId: "guestvista-4308f",
-    storageBucket: "guestvista-4308f.appspot.com",
-    messagingSenderId: "935264390170",
-    appId: "1:935264390170:web:1b74417f1a14f7609f95a3",
-    measurementId: "G-RVEK214LJ7"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  
-// Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app);
-
-
-
-  export default auth;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
