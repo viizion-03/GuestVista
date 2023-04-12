e6c8ffbc54000237e5146ae91070c9fac6ae33f8
Kevin <125473461+viizion-03@users.noreply.github.com>
2023-04-12 08:27:13 +0200

    UI for listing guesthouses

---
+++ src/components/GuestHouseTile.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-regular-svg-icons';
+import { faMapMarker } from '@fortawesome/free-solid-svg-icons';
+
+const GuestHouseTile = (props) => {
+
+    const navigate = useNavigate()
+
+    function seeGuestHouses() {
+      navigate('/guesthouse')
+    }
+
+  return (
+    <div className='ghl--house-card'>
+    <span className='house-card-image'>
+    <img src={props.img} alt='guesthouse' />
+    </span>
+
+    <div className='house-card-details'>
+      <span className='house-card-header'>
+        <h4>{props.name}</h4>
+        <h3>P{props.price}</h3>
+      </span>
+
+      <div className='house-card-rating'>
+        {<FontAwesomeIcon icon={faStar} color='#FFD700' />}
+        {<FontAwesomeIcon icon={faStar} color='#FFD700' />}
+        {<FontAwesomeIcon icon={faStar} color='#FFD700' />}
+        {<FontAwesomeIcon icon={faStar} color='#FFD700' />}
+        {<FontAwesomeIcon icon={faStar} color='#FFD700' />}
+        <h4>{props.rating}</h4>
+      </div>
+
+      <span className='house-card-distance'>
+        <FontAwesomeIcon icon={faMapMarker} />
+        <h4>{props.distance} km away</h4>
+      </span>
+
+      <p className='house-card-description'>
+        {props.description}
+      </p>
+
+      <button className='ghl--view-house-btn' onClick={seeGuestHouses}>View Details</button>
+    </div>
+  </div>
+  )
+}
+
+export default GuestHouseTile
