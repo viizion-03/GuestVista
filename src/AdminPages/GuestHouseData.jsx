5d1d93b2f16b2975871f820ac062cd05741432ec
KaraboKelly <125416088+KaraboKelly@users.noreply.github.com>
2023-04-16 08:43:41 +0200

    Delete src/AdminPages directory

--- src/AdminPages/GuestHouseData.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const GuestHouseData = () => {
-  return (
-    <div>GuestHouseData</div>
-  )
-}
-
-export default GuestHouseData
--- src/AdminPages/Subcribers.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Subcribers = () => {
-  return (
-    <div>Subcribers</div>
-  )
-}
-
-export default Subcribers
