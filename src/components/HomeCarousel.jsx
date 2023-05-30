485ae9369e55924ff20b4ea29daed31c36c6c936
viizion-03 <ST20001354@Studentmail.BIUST.AC.BW>
2023-05-30 14:59:29 +0200

    trying to fix carasouel

--- src/components/HomeCarousel.jsx
+++ src/components/HomeCarousel.jsx
@@ -25,7 +25,7 @@ const HomeCarousel = () => {
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="https://firebasestorage.googleapis.com/v0/b/guestvista-4308f.appspot.com/o/carousel%2Fr-architecture-2gDwlIim3Uw-unsplash%20%5BMConverter.eu%5D.webp?alt=media&token=24132dae-903a-4b7d-8f47-0ab62799ddfe"
+                        src="https://firebasestorage.googleapis.com/v0/b/react-project-5130e.appspot.com/o/carousel%2Fr-architecture-2gDwlIim3Uw-unsplash%20%5BMConverter.eu%5D.webp?alt=media&token=bb8442bf-7056-40e0-b214-db18c458cee4"
                         alt="First slide"
                         style={styles}
                     />
@@ -37,7 +37,7 @@ const HomeCarousel = () => {
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="https://firebasestorage.googleapis.com/v0/b/guestvista-4308f.appspot.com/o/carousel%2Fvu-anh-TiVPTYCG_3E-unsplash%20%5BMConverter.eu%5D.webp?alt=media&token=1430cab4-8cd4-4afa-8694-85a5dff8aea7"
+                        src="https://firebasestorage.googleapis.com/v0/b/react-project-5130e.appspot.com/o/carousel%2Fvu-anh-TiVPTYCG_3E-unsplash%20%5BMConverter.eu%5D.webp?alt=media&token=5cffe46d-2d89-4ee3-8fa5-50d660efd7a4"
                         alt="First slide"
                         style={styles}
                     />
@@ -49,7 +49,7 @@ const HomeCarousel = () => {
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="https://firebasestorage.googleapis.com/v0/b/guestvista-4308f.appspot.com/o/carousel%2Fzero-take-QzQe16eKc1I-unsplash%20%5BMConverter.eu%5D.webp?alt=media&token=23abc247-a128-45d4-a412-815f6646d34a"
+                        src="https://firebasestorage.googleapis.com/v0/b/react-project-5130e.appspot.com/o/carousel%2Fzero-take-Z1Y8kWBe6C8-unsplash%20%5BMConverter.eu%5D.webp?alt=media&token=caac80fc-57c9-42fe-984e-3d7f53c1059e"
                         alt="First slide"
                         style={styles}
                     />
